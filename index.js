a146555af6214f37d9ad66ff90dc06021970b80a
Damian Randall <damian.randall@blackrock.com>
2019-03-20 18:59:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,9 +15,4 @@ function whisper(string) {
     logWhisper('HELLO')
 
     expect(spy).toHaveBeenCalledWith('hello')
-    console.log.restore()*/
-
-function sayHiToGrandma(string) {
-  return string console.log().toLowerCase;
-}
-string "I can't hear you!"
+    console.log.restore()*
