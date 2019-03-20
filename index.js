8cc3bd9a83e79a57a4ec3b5079ab39c25f51b861
Damian Randall <damian.randall@blackrock.com>
2019-03-20 18:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+/*Returns a string with uppercase values*/
 function shout(string) {
   return string.toUpperCase()
+  
 }
+/*Returns a string with lowercase values */
 function whisper(string) {
   return string.toLowerCase()
-}
+  }
+  /*
 function sayHiToGrandma(console.log) {
   console.log.restore() string "I can't hear you!"
 }
